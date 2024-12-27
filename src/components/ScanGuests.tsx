83a32ab192e254dd7a1ae75eb6773b35a49ed3e5
gabrielvm98 <64874245+gabrielvm98@users.noreply.github.com>
2024-12-26 22:24:05 -0500

    Leer QRs

--- src/components/ScanGuests.tsx
+++ src/components/ScanGuests.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import { Button } from "@/components/ui/button"
 import { QrReader } from 'react-qr-reader'
 
 interface ScanQRTabProps {
@@ -10,52 +9,30 @@ interface ScanQRTabProps {
 
 export function ScanQRTab({ eventId }: ScanQRTabProps) {
   const [isScanning, setIsScanning] = useState(false)
-  const [scannedData, setScannedData] = useState<string | null>(null)
 
   const startScanning = () => {
     setIsScanning(true)
-    setScannedData(null)
   }
 
   const stopScanning = () => {
     setIsScanning(false)
   }
 
-  const handleScan = (result: string | null) => {
-    if (result) {
-      console.log('QR Code scanned:', result, eventId)
-      setScannedData(result)
-      stopScanning()
-      // TODO lógica para procesar el código QR      
-    }
-  }
-
-  /*const handleError = (error: Error) => {
-    console.error('Error al escanear el código QR:', error)
-  }*/
-
   return (
     <div className="space-y-4">
       {isScanning ? (
-        <div className="space-y-4">
+        <div>
           <QrReader
             constraints={{ facingMode: 'environment' }}
-            onResult={(result) => result && handleScan(result.getText())}
+            onResult={(result) => result && console.log('QR Code scanned:', result.getText(), eventId)}
             containerStyle={{ width: '100%', maxWidth: '500px', margin: '0 auto' }}
             videoStyle={{ width: '100%', height: 'auto' }}
           />
-          <Button onClick={stopScanning} className="w-full">Detener Escaneo</Button>
+          <button onClick={stopScanning} className="w-full bg-red-500 text-white py-2 mt-4 rounded">Detener Escaneo</button>
         </div>
       ) : (
-        <Button onClick={startScanning} className="w-full">Iniciar Escaneo de QR</Button>
-      )}
-      {scannedData && (
-        <div className="mt-4 p-4 bg-green-100 border border-green-300 rounded">
-          <p className="font-bold">Código QR escaneado:</p>
-          <p>{scannedData}</p>
-        </div>
+        <button onClick={startScanning} className="w-full bg-green-500 text-white py-2 mt-4 rounded">Iniciar Escaneo de QR</button>
       )}
     </div>
   )
 }
-
